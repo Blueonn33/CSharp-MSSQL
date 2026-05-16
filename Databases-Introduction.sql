8f733fe8950b4f402042f8ff010b979db8765808
Martin Marinov <m.marinov1733@gmail.com>
2026-05-16 22:20:35 +0300

    users table

--- Databases-Introduction.sql
+++ Databases-Introduction.sql
@@ -1,9 +1,7 @@
 -- 1
-
 CREATE DATABASE [Minions]
 
 -- 2
-
 CREATE TABLE [Minions]
 (
 	[Id] INT PRIMARY KEY,
@@ -17,12 +15,10 @@ CREATE TABLE [Towns]
 );
 
 -- 3
-
 ALTER TABLE [Minions]
 ADD [TownId] INT FOREIGN KEY REFERENCES [Towns]([Id]) NOT NULL
 
 -- 4
-
 INSERT INTO [Towns]([Id], [Name])
 VALUES (1, 'Sofia'),
 	   (2, 'Plovdiv'),
@@ -33,18 +29,26 @@ VALUES (1, 'Kevin', 22, 1),
 	   (3, 'Steward', NULL , 2);
 
 -- 5
-
 TRUNCATE TABLE [Minions]
 
 -- 6 
-
 DROP TABLE [Minions]
 DROP TABLE [Towns]
-
 
 -- 7 
 
 -- 8
+-- CHECK Constraint: User-defined rule for the value in a column/s
+CREATE TABLE [Users] 
+(
+	[Id] BIGINT PRIMARY KEY	IDENTITY(1, 1),
+	[Name] VARCHAR(30) UNIQUE NOT NULL,
+	[Password] VARCHAR(26) NOT NULL,
+	[ProfilePicture] VARBINARY(MAX),
+	CHECK(LEN([ProfilePicture]) <= 921600), -- Up to 900KB
+	[LastLoginTime] DATETIME2,
+	[IsDeleted] BIT NOT NULL
+);
 
 -- 9
 
