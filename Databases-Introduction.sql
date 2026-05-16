c442326fa7ca62ab2a2206733fabf90eba1e0a71
Martin Marinov <m.marinov1733@gmail.com>
2026-05-17 00:16:13 +0300

    определени имена

--- Databases-Introduction.sql
+++ Databases-Introduction.sql
@@ -119,6 +119,21 @@ ORDER BY [Salary]
 	DESC
 
 -- 21
+  SELECT [Name] 
+    FROM [Towns]
+ORDER BY [Name]
+
+  SELECT [Name] 
+    FROM [Departments]
+ORDER BY [Name]
+
+  SELECT [FirstName],
+		 [LastName],
+		 [JobTitle],
+		 [Salary]
+    FROM [Employees]
+ORDER BY [Salary]
+	DESC
 
 -- 22
 
