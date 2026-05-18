742a5c7eff64622c62d6e3d6137fc778d61919b1
Martin Marinov <m.marinov1733@gmail.com>
2026-05-18 12:02:56 +0300

    07

--- 02-CRUD-Databases/CRUD.sql
+++ 02-CRUD-Databases/CRUD.sql
@@ -79,3 +79,8 @@ SELECT FirstName, MiddleName, LastName FROM Employees
 SELECT FirstName + '.' + LastName + '@softuni.bg' 
 	AS "Full Email Address"
 	FROM Employees
+
+-- 07
+
+SELECT DISTINCT Salary
+FROM Employees
