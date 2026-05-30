53f3b43c6ea19fa2e46048fbf4d65c9a20606f3d
Martin Marinov <m.marinov1733@gmail.com>
2026-05-31 00:33:07 +0300

    box, pallet

--- Build-In-Functions.sql
+++ Build-In-Functions.sql
@@ -5,3 +5,12 @@ SELECT CONCAT(SUBSTRING('8958329213', 1, LEN('8958329213') - 4), REPLICATE('*', 
 SELECT FORMAT(8.675, 'C2', 'bg-BG')
 
 SELECT CHARINDEX('Uni', 'SoftUni')
+
+------------------
+
+SELECT 
+		Id,
+		CEILING(
+			CAST(CEILING(CAST(Quantity AS FLOAT) / BoxCapacity) AS FLOAT) / PalletCapacity
+		)
+		FROM Products
