3713ee533fc3023c3bee4f36b27f6a1d7d52152b
Martin Marinov <m.marinov1733@gmail.com>
2026-05-30 16:22:25 +0300

    models

--- Table-Relations.sql
+++ Table-Relations.sql
@@ -55,3 +55,12 @@ VALUES
 		('BMW', '1916-03-07'),
 		('Tesla', '2003-01-01'),
 		('Lada', '1966-05-01')
+
+INSERT INTO Models ([Name], ManufacturerID)
+VALUES	
+		('X1', 1),
+		('i6', 1),
+		('Model S', 2),
+		('Model X', 2),
+		('Model 3', 2),
+		('Nova', 3)
