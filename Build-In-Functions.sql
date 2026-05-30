92de93b4a5259e43b8d4bc91bd1d4c3e8637e6cb
Martin Marinov <m.marinov1733@gmail.com>
2026-05-30 22:41:24 +0300

    string

---
+++ Build-In-Functions.sql
@@ -0,0 +1,7 @@
+SELECT 'Пешо' AS [Name], UPPER('Пешо') AS NormalizedName
+
+SELECT CONCAT(SUBSTRING('8958329213', 1, LEN('8958329213') - 4), REPLICATE('*', 4))
+
+SELECT FORMAT(8.675, 'C2', 'bg-BG')
+
+SELECT CHARINDEX('Uni', 'SoftUni')
