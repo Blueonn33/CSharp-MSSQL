139847828c74c93f20a1a981211adeddf148c5a7
Martin Marinov <m.marinov1733@gmail.com>
2026-05-30 18:27:23 +0300

    teachers

--- Table-Relations.sql
+++ Table-Relations.sql
@@ -106,3 +106,12 @@ VALUES
 		(2, 102),
 		(2, 103),
 		(3, 103)
+
+-- 04
+CREATE TABLE Teachers
+(
+	TeacherID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] VARCHAR(50) NOT NULL,
+	ManagerID INT FOREIGN KEY REFERENCES Teachers(TeacherID) NULL
+);
+
