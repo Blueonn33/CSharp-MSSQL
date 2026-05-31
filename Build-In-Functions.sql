72f2c3bda928280d7ede8288aa769050156886c0
Martin Marinov <m.marinov1733@gmail.com>
2026-05-31 21:40:01 +0300

    games

--- Build-In-Functions.sql
+++ Build-In-Functions.sql
@@ -198,3 +198,23 @@ SELECT p.PeakName,
 	AS r
  WHERE RIGHT(p.PeakName, 1) = LEFT(r.RiverName, 1)
 ORDER BY Mix
+
+-- 17
+SELECT [Name],
+	   CASE
+			WHEN DATEPART(HOUR, [Start]) BETWEEN 0 AND 11 THEN 'Morning'
+			WHEN DATEPART(HOUR, [Start]) BETWEEN 12 AND 17 THEN 'Afternoon'
+			ELSE 'Evening'
+	   END
+    AS [Part of the Day],
+	   CASE
+			WHEN Duration <= 3 THEN 'Extra Short'
+			WHEN Duration BETWEEN 4 AND 6 THEN 'Short'
+			WHEN Duration > 6 THEN 'Long'
+			WHEN Duration IS NULL THEN 'Extra Long'
+		END
+	AS  Duration
+  FROM Games AS g
+ORDER BY g.[Name], 
+		 Duration,
+		 [Part of the Day]
