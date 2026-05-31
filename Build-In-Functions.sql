fa1203addff23f28f1ad98292aa3d0caadef91fa
Martin Marinov <m.marinov1733@gmail.com>
2026-05-31 19:28:03 +0300

    rank 2

--- Build-In-Functions.sql
+++ Build-In-Functions.sql
@@ -171,3 +171,18 @@ ORDER BY [Name]
     FROM Employees
    WHERE Salary BETWEEN 10000 AND 50000
 ORDER BY Salary DESC
+
+-- 11
+ SELECT *
+   FROM (
+					SELECT EmployeeID,
+					 FirstName,
+					 LastName,
+					 Salary,
+					 DENSE_RANK() OVER(PARTITION BY Salary ORDER BY EmployeeID) 
+				  AS [Rank]
+				FROM Employees
+			   WHERE Salary BETWEEN 10000 AND 50000
+		) AS EmployeesSalaryRank
+   WHERE [Rank] = 2
+ORDER BY Salary DESC		
