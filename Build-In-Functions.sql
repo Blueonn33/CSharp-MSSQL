651d1d92a9c2e1a6051c941f1f171a1b3e98cf69
Martin Marinov <m.marinov1733@gmail.com>
2026-05-31 23:16:53 +0300

    email

--- Build-In-Functions.sql
+++ Build-In-Functions.sql
@@ -242,6 +242,12 @@ FROM Games
 WHERE YEAR([Start]) IN (2011, 2012)
 ORDER BY [Start], [Name]
 
+-- 15
+SELECT Username, SUBSTRING(Email, CHARINDEX('@', Email) + 1, LEN(Email) - CHARINDEX('@', Email)) AS [Email Provider]
+FROM Users
+ORDER BY [Email Provider], Username
+
+-- SUBSTRING(Email, 1, CHARINDEX(Email, '@'))
 -- 17
 SELECT [Name],
 	   CASE
