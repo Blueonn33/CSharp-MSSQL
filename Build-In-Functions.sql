448d9535e78810b2fd37e63b064710485c279164
Martin Marinov <m.marinov1733@gmail.com>
2026-05-31 17:55:08 +0300

    LEN

--- Build-In-Functions.sql
+++ Build-In-Functions.sql
@@ -139,3 +139,8 @@ SELECT [Name]
 FROM Towns
 WHERE [Name] LIKE '_____' OR [Name] LIKE '______'
 ORDER BY [Name]
+
+SELECT [Name] 
+FROM Towns
+WHERE LEN([Name]) = 5 OR LEN([Name]) = 6
+ORDER BY [Name]
