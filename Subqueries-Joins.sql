6ee124358af9828b1fe0f71e05fa6db3db590939
Martin Marinov <m.marinov1733@gmail.com>
2026-06-02 20:06:13 +0300

    inner join

--- Subqueries-Joins.sql
+++ Subqueries-Joins.sql
@@ -62,3 +62,11 @@ FROM Employees
 
 SELECT * FROM ##Employees
 
+------------------
+
+-- 04
+SELECT TOP 5 EmployeeID, FirstName, Salary, d.[Name] AS DepartmentName
+FROM Employees AS e
+INNER JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE e.Salary > 15000
+ORDER BY d.DepartmentID
