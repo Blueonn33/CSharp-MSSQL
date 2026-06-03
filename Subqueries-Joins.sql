c1db374839015c503683fec50cbf59ed7fdce66c
Martin Marinov <m.marinov1733@gmail.com>
2026-06-03 17:57:34 +0300

    колко е високо

--- Subqueries-Joins.sql
+++ Subqueries-Joins.sql
@@ -179,6 +179,24 @@ JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
 GROUP BY d.DepartmentID
 ORDER BY MinAverageSalary
 
+-- 12
+SELECT * FROM Countries
+SELECT * FROM Mountains
+SELECT * FROM Peaks
+SELECT * FROM MountainsCountries
+
+SELECT 
+		c.CountryCode,
+		m.MountainRange,
+		p.PeakName,
+		p.Elevation
+FROM Countries AS c
+JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+JOIN Mountains AS m ON m.Id = mc.MountainId
+JOIN Peaks AS p ON p.MountainId = m.Id
+WHERE c.CountryCode = 'BG' AND p.Elevation > 2835
+ORDER BY p.Elevation DESC
+
 -- 13
 SELECT c.CountryCode,
 	   COUNT(MountainID) AS MountainRanges
