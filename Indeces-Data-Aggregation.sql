b46c9baae5e99ce8c5e8601d1d454bcc5eb32adb
Martin Marinov <m.marinov1733@gmail.com>
2026-06-03 21:54:15 +0300

    count

--- Indeces-Data-Aggregation.sql
+++ Indeces-Data-Aggregation.sql
@@ -1,3 +1,8 @@
 SELECT DepartmentID, AVG(Salary) AS AvgSalary      
 FROM Employees
 GROUP BY DepartmentID
+
+SELECT DepartmentID, Salary, COUNT(*) AS Count      
+FROM Employees
+GROUP BY DepartmentID, Salary
+ORDER BY DepartmentID, Salary DESC
