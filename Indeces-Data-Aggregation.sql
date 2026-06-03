323c03a56988f622968d2774ff11097947ea69e2
Martin Marinov <m.marinov1733@gmail.com>
2026-06-03 21:57:27 +0300

    total salary

--- Indeces-Data-Aggregation.sql
+++ Indeces-Data-Aggregation.sql
@@ -6,3 +6,10 @@ SELECT DepartmentID, Salary, COUNT(*) AS Count
 FROM Employees
 GROUP BY DepartmentID, Salary
 ORDER BY DepartmentID, Salary DESC
+
+SELECT 
+		DepartmentID,
+		SUM(Salary) AS TotalSalary
+FROM Employees
+GROUP BY DepartmentID
+ORDER BY 1 -- DepartmentID
