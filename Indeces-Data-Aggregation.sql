5fabf89a50d36d71d5d040cbc68f94da4d753133
Martin Marinov <m.marinov1733@gmail.com>
2026-06-04 13:08:44 +0300

    150K

--- Indeces-Data-Aggregation.sql
+++ Indeces-Data-Aggregation.sql
@@ -104,6 +104,16 @@ FROM WizzardDeposits
 GROUP BY DepositGroup, MagicWandCreator
 HAVING MagicWandCreator = 'Ollivander family'
 
+-- 07
+SELECT 
+		DepositGroup,
+		SUM(DepositAmount) AS TotalSum
+FROM WizzardDeposits
+GROUP BY DepositGroup, MagicWandCreator
+HAVING MagicWandCreator = 'Ollivander family' 
+	AND SUM(DepositAmount) < 150000
+ORDER BY TotalSum DESC
+
 -- 09
 SELECT *
 FROM WizzardDeposits
