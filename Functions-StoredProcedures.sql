f8a0bca6f7db8695f86ef2d7f29228360a72c28d
Martin Marinov <m.marinov1733@gmail.com>
2026-06-06 12:47:07 +0300

    table-valued function

--- Functions-StoredProcedures.sql
+++ Functions-StoredProcedures.sql
@@ -15,3 +15,23 @@ SELECT
 		[Name],
 		dbo.udf_ProjectDurationInWeeks(StartDate, EndDate) AS ProjectDuration
 FROM Projects
+
+-------------
+-- Трябва да се сложи на отделно Query
+
+CREATE FUNCTION udf_AverageSalary(@DepartmentName VARCHAR(50))
+RETURNS TABLE
+AS
+RETURN
+(
+		SELECT 
+				d.[Name] AS DepartmentName,
+				AVG(e.Salary) AS AverageSalary
+		FROM Departments AS d
+		JOIN Employees AS e ON d.DepartmentID = e.DepartmentID
+		WHERE d.[Name] = @DepartmentName
+		GROUP BY d.[Name]
+)
+
+SELECT *
+FROM udf_AverageSalary('Sales')
