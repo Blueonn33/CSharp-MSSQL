bbf08e861d63bb2a51e771a37f7e589b54728aaf
Martin Marinov <m.marinov1733@gmail.com>
2026-06-06 22:55:48 +0300

    above num

--- Functions-StoredProcedures.sql
+++ Functions-StoredProcedures.sql
@@ -166,3 +166,16 @@ BEGIN
 END
 
 GO
+
+-- 02
+CREATE PROCEDURE usp_GetEmployeesSalaryAboveNumber (@minSalary DECIMAL(18,4)) 
+AS
+BEGIN
+		SELECT 
+				FirstName,
+				LastName
+		FROM Employees
+		WHERE Salary >= @minSalary
+END
+
+EXEC usp_GetEmployeesSalaryAboveNumber 48100
