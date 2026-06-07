8b7df44bdc6fb4b13f932762d253f54280db9854
Martin Marinov <m.marinov1733@gmail.com>
2026-06-07 16:48:51 +0300

    salary

--- Functions-StoredProcedures.sql
+++ Functions-StoredProcedures.sql
@@ -213,7 +213,32 @@ END
 EXEC usp_GetEmployeesFromTown 'Sofia'
 
 GO
+
 -- 05
+GO
+
+CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4))
+RETURNS VARCHAR(10) AS
+BEGIN
+		DECLARE @result AS VARCHAR(10)
+
+		IF (@salary < 30000)
+			SET @result = 'Low'
+		ELSE IF (@salary <= 50000)
+			SET @result = 'Average'
+		ELSE
+			SET @result = 'High'
+
+		RETURN @result
+END
+
+SELECT 
+		Salary,
+		dbo.ufn_GetSalaryLevel(Salary) AS [Salary Level]
+FROM Employees
+
+GO
+
 -- 06
 -- 07
 GO
