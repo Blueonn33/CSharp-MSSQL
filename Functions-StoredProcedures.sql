224b13087f5636941ee2451ea3fc4534297c8d3c
Martin Marinov <m.marinov1733@gmail.com>
2026-06-07 17:35:18 +0300

    5 years

--- Functions-StoredProcedures.sql
+++ Functions-StoredProcedures.sql
@@ -428,6 +428,33 @@ END
 
 SELECT dbo.ufn_CalculateFutureValue(1000, 0.1, 5)
 
+-- 12
+GO
+
+CREATE OR ALTER PROCEDURE usp_CalculateFutureValueForAccount (
+	@accountId INT, 
+	@interestRate FLOAT
+) AS
+BEGIN
+	SELECT 
+			@accountId AS [Account Id],
+			FirstName AS [Fist Name],
+			LastName AS [Last Name],
+			Balance AS [Current Balance],
+			dbo.ufn_CalculateFutureValue(Balance, @interestRate , 5) AS [Balance in 5 years]
+			FROM AccountHolders AS ah
+			JOIN Accounts AS a ON @accountId = a.Id
+			WHERE ah.Id = @accountId
+END
+
+EXEC usp_CalculateFutureValueForAccount 1, 0.1
+
+SELECT *
+FROM AccountHolders
+
+GO
+
+
 -- 13
 GO
 
