e5085e9e46230fab5bece277722fba01cea26398
Martin Marinov <m.marinov1733@gmail.com>
2026-06-10 23:56:21 +0300

    order books

--- ExamPreparation.sql
+++ ExamPreparation.sql
@@ -124,3 +124,11 @@ WHERE [Name] = 'Alex Michaelides'
 
 ---------------------------------
 
+-- 05
+
+SELECT 
+		Title AS [Book Title],
+		ISBN,
+		YearPublished AS YearReleased
+FROM Books
+ORDER BY YearPublished DESC, Title ASC
